74dd9ec9fbf1ff5a1fea3c95d5560c447039fbfc
Yixiang Xu <yixiangxu@umass.edu>
2017-09-15 15:31:02 -0400

    add comments

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -17,21 +17,25 @@ public class Block
         tiles = new Tile[newWidth, newHeight];
     }
 
+    //Return the width of the block
     public int GetWidth()
     {
         return width;
     }
 
+    //Return the height of the block
     public int GetHeight()
     {
         return height;
     }
 
+    //Return the collections of Tiles of the block
     public Tile[,] GetTiles()
     {
         return tiles;
     }
 
+    //Set the Tile of given indexes x and y to new Tile
     public void SetTile(int x, int y, Tile newTile)
     {
         tiles[x, y] = newTile;
