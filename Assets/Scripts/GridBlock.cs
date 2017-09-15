741d4755a939efeee2dd3d844da3ffb1f5e5cd4b
Yixiang Xu <yixiangxu@umass.edu>
2017-09-15 15:11:41 -0400

    add Constructor and move method

--- Assets/Scripts/GridBlock.cs
+++ Assets/Scripts/GridBlock.cs
@@ -10,9 +10,15 @@ public class GridBlock
 
     int y;
 
-    GridBlock(int xStart, int yStart, Block myBlock)
-    {
+    Block block;
 
+    Grid grid;
+
+    public GridBlock(int xStart, int yStart, Block myBlock)
+    {
+        x = xStart;
+        y = xStart;
+        block = myBlock;
     }
 
     public int GetX()
@@ -27,6 +33,44 @@ public class GridBlock
 
     public void Move(Enums.Direction direction)
     {
+        int tempX = x;
+        int tempY = y;
+        int blockW = block.GetWidth();
+        int blockH = block.GetHeight();
+        Tile[,] gridTiles = grid.GetTiles();
 
+        switch (direction)
+        {
+            case Enums.Direction.Down:
+                y += blockH;
+                break;
+            case Enums.Direction.Up:
+                y -= blockH;
+                break;
+            case Enums.Direction.Left:
+                x -= blockW;
+                break;
+            case Enums.Direction.Right:
+                x += blockW;
+                break;
+        }
+
+        if (grid.CanBlockFit(x, y, block))
+        {
+            Tile[,] blockTiles = block.GetTiles();
+            for (int i = 0; i < blockW; i++)
+            {
+                for (int j = 0; j < blockH; j++)
+                {
+                    gridTiles[x + i, y + j].Duplicate(blockTiles[i, j]);
+                    blockTiles[x + i, y + j].Clear();
+                }
+            }
+        }
+        else
+        {
+            x = tempX;
+            y = tempY;
+        }
     }
 }
