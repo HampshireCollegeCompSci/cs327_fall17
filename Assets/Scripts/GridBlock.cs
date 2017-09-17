b52d603bbf13da98d4701a5fbb01a648a11d1661
Yixiang Xu <yixiangxu@umass.edu>
2017-09-17 11:05:15 -0400

    revise move method

--- Assets/Scripts/GridBlock.cs
+++ Assets/Scripts/GridBlock.cs
@@ -33,51 +33,197 @@ public class GridBlock
         return y;
     }
 
-    /*Enable gridblocks to move according to their directions.
-     Gridblock moves if it can fit into the next position.The 
-     moving process is not physical. Instead, it duplicates
-     all the tiles to the next position and erases the original
-     ones.*/
+    /*Repeatedly moves the GridBlock along the Grid in
+     *the given direction, one Tile at a time, only stopping
+     *at occupied Tiles (preventing Tile overlap) or the
+     *border.*/
     public void Move(Enums.Direction direction)
     {
-        int tempX = x;
-        int tempY = y;
-        int blockW = block.GetWidth();
-        int blockH = block.GetHeight();
         Tile[,] gridTiles = grid.GetTiles();
+        Tile[,] blockTiles = block.GetTiles();
+        List<Vector2> exTilesIndex = new List<Vector2>();
+        bool movable = true;
 
         switch (direction)
         {
-            case Enums.Direction.Down:
-                y += blockH;
+            case Enums.Direction.Right:
+                if (x + block.GetWidth() + 1 > grid.GetWidth())
+                    movable = false;
+
+                if (movable)
+                {
+                    for (int i = 0; i < block.GetHeight(); i++)
+                    {
+                        int maxX = -1;
+                        for (int j = 0; j < block.GetWidth(); j++)
+                        {
+                            if (blockTiles[j, i].GetIsOccupied())
+                                maxX = j;
+                        }
+
+                        if (blockTiles[maxX + 1, i].GetIsOccupied())
+                        {
+                            movable = false;
+                            break;
+                        }
+
+                        if (maxX != -1)
+                            exTilesIndex.Add(new Vector2(maxX, i));
+                    }
+                }
+
+                if (movable)
+                {
+                    for (int i = 0; i < exTilesIndex.Count; i++)
+                    {
+                        int exTileX = (int)exTilesIndex[i].x;
+                        int exTileY = (int)exTilesIndex[i].y;
+
+                        for (int j = exTileX; j >= 0; j--)
+                        {
+                            gridTiles[j + x + 1, y + exTileY].Duplicate(blockTiles[j, exTileY]);
+                            gridTiles[j + x, y + exTileY].Clear();
+                        }
+                    }
+
+                    x += 1;
+                }
+
                 break;
-            case Enums.Direction.Up:
-                y -= blockH;
+            case Enums.Direction.Down:
+                if (y + block.GetHeight() + 1 > grid.GetHeight())
+                    movable = false;
+
+                if (movable)
+                {
+                    for (int i = 0; i < block.GetWidth(); i++)
+                    {
+                        int maxY = -1;
+                        for (int j = 0; j < block.GetHeight(); j++)
+                        {
+                            if (blockTiles[i, j].GetIsOccupied())
+                                maxY = j;
+                        }
+
+                        if (blockTiles[i, maxY + 1].GetIsOccupied())
+                        {
+                            movable = false;
+                            break;
+                        }
+
+                        if (maxY != -1)
+                            exTilesIndex.Add(new Vector2(i, maxY));
+                    }
+                }
+
+                if (movable)
+                {
+                    for (int i = 0; i < exTilesIndex.Count; i++)
+                    {
+                        int exTileX = (int)exTilesIndex[i].x;
+                        int exTileY = (int)exTilesIndex[i].y;
+
+                        for (int j = exTileY; j >= 0; j--)
+                        {
+                            gridTiles[x + exTileX, y + j + 1].Duplicate(blockTiles[exTileX, j]);
+                            gridTiles[x + exTileX, y + j].Clear();
+                        }
+                    }
+
+                    y += 1;
+                }
+
                 break;
             case Enums.Direction.Left:
-                x -= blockW;
-                break;
-            case Enums.Direction.Right:
-                x += blockW;
-                break;
-        }
+                if (x - 1 < 0)
+                    movable = false;
 
-        if (grid.CanBlockFit(x, y, block))
-        {
-            Tile[,] blockTiles = block.GetTiles();
-            for (int i = 0; i < blockW; i++)
-            {
-                for (int j = 0; j < blockH; j++)
+                if (movable)
                 {
-                    gridTiles[x + i, y + j].Duplicate(blockTiles[i, j]);
-                    blockTiles[x + i, y + j].Clear();
+                    for (int i = 0; i < block.GetHeight(); i++)
+                    {
+                        int minX = -1;
+                        for (int j = block.GetWidth() - 1; j >= 0; j--)
+                        {
+                            if (blockTiles[j, i].GetIsOccupied())
+                                minX = j;
+                        }
+
+                        if (blockTiles[minX - 1, i].GetIsOccupied())
+                        {
+                            movable = false;
+                            break;
+                        }
+
+                        if (minX != -1)
+                            exTilesIndex.Add(new Vector2(minX, i));
+                    }
                 }
-            }
-        }
-        else
-        {
-            x = tempX;
-            y = tempY;
+
+                if (movable)
+                {
+                    for (int i = 0; i < exTilesIndex.Count; i++)
+                    {
+                        int exTileX = (int)exTilesIndex[i].x;
+                        int exTileY = (int)exTilesIndex[i].y;
+
+                        for (int j = 0; j < exTileX; j++)
+                        {
+                            gridTiles[j + x - 1, y + exTileY].Duplicate(blockTiles[j, exTileY]);
+                            gridTiles[j + x, y + exTileY].Clear();
+                        }
+                    }
+
+                    x -= 1;
+                }
+
+                break;
+
+            case Enums.Direction.Up:
+                if (y - 1 < 0)
+                    movable = false;
+
+                if (movable)
+                {
+                    for (int i = 0; i < block.GetWidth(); i++)
+                    {
+                        int minY = -1;
+                        for (int j = block.GetHeight() - 1; j >= 0; j--)
+                        {
+                            if (blockTiles[i, j].GetIsOccupied())
+                                minY = j;
+                        }
+
+                        if (blockTiles[i, minY - 1].GetIsOccupied())
+                        {
+                            movable = false;
+                            break;
+                        }
+
+                        if (minY != -1)
+                            exTilesIndex.Add(new Vector2(i, minY));
+                    }
+                }
+
+                if (movable)
+                {
+                    for (int i = 0; i < exTilesIndex.Count; i++)
+                    {
+                        int exTileX = (int)exTilesIndex[i].x;
+                        int exTileY = (int)exTilesIndex[i].y;
+
+                        for (int j = 0; j < exTileY; j++)
+                        {
+                            gridTiles[x + exTileX, y + j - 1].Duplicate(blockTiles[exTileX, j]);
+                            gridTiles[x + exTileX, y + j].Clear();
+                        }
+                    }
+
+                    y -= 1;
+                }
+
+                break;
+            
         }
     }
 }
