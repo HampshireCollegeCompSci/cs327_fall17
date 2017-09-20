9bf4c2d05565371c4b09fac83286f5d7005758ec
Maia Doerner <wc7642@gmail.com>
2017-09-20 10:48:55 -0400

    added tile class

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,4 +1,5 @@
-﻿// Author(s): Paul Calande, [your name here]
+﻿// Author(s): Paul Calande, Maia Doerner
+
 
 using System.Collections;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ public class Tile : MonoBehaviour
 {
     bool isOccupied = false;
 
+
     public enum TileType
     {
         Regular,
@@ -15,30 +17,41 @@ public class Tile : MonoBehaviour
         Vestige
     }
 
-    TileType type;
+	TileType type;
+	Sprite spriteFilled;
+	Sprite empty;
+	SpriteRenderer spriteRenderer;
+
 
     public bool GetIsOccupied()
     {
         return isOccupied;
     }
-
+		
     public void Fill()
     {
-
+		//this.type = TileType.Regular;
+		this.spriteRenderer.sprite = spriteFilled;
+		this.isOccupied = true;
     }
 
     public void Clear()
     {
-
+		//Tile.Destroy;
+		this.spriteRenderer.sprite = empty;
+		this.isOccupied = false;
     }
 
     public void Duplicate(Tile other)
     {
-
+		this.isOccupied = other.isOccupied;
+		this.spriteRenderer.sprite = other.spriteRenderer.sprite;
+		this.type = other.type;
     }
 
     public void SetType(TileType newType)
     {
+		this.type = newType;
 
     }
 
