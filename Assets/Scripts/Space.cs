49bd1c58b6ecc264dc12c43df41392b44af56b4d
Paul Calande <paul.luap09@gmail.com>
2017-09-20 01:39:29 -0400

    fix some errors, adjust some skeleton methods

--- Assets/Scripts/Space.cs
+++ Assets/Scripts/Space.cs
@@ -1,4 +1,4 @@
-﻿// Author: Wm. Josiah Erikson
+﻿// Author(s): Paul Calande, Wm. Josiah Erikson
 
 using System.Collections;
 using System.Collections.Generic;
@@ -6,36 +6,39 @@ using UnityEngine;
 
 public class Space : MonoBehaviour
 {
-	private int xcoord; //the x-coordinate of this space
-	private int ycoord; //the y-coordinate if this space
+	private int x; //the x-coordinate of this space
+	private int y; //the y-coordinate of this space
 	private int width; //width of this space
 	private int height; //height of this space
 	private Grid grid; //which grid this space is on
 
 	public void Init (int mx, int my, int mwidth, int mheight, Grid mgrid)
 	{
-        xcoord = mx;
-        ycoord = my;
+        x = mx;
+        y = my;
         width = mwidth;
         height = mheight;
         grid = mgrid;
-
 	}
 
-	public bool CanBlockFit (Block block)
-	{
+    public bool CanBlockFit(Block block)
+    {
         if (block.GetWidth() == width && block.GetHeight() == height)
         {
-            return true; // It fits
+            // It fits, dimensionally speaking.
+            return grid.CanBlockFit(x, y, block); // Check if the important Cells are empty.
         }
-        else { return false; } //It doesn't}
-	}
+        else
+        {
+            // It doesn't.
+            return false;
+        }
+    }
 
-		//This method should be called when a DraggableBlock is dragged onto this Space.
-		public void PlaceBlock (Block block)
+	//This method should be called when a DraggableBlock is dragged onto this Space.
+	public void PlaceBlock(DraggableBlock block)
 	{
-        grid.WriteBlock(block); //We're placing this block. Apparently it's the external responsibility to make sure this will work
-        DestroyObject(block); //And now we're done with this GameObject - it's on the grid
-
-	}
+        grid.WriteBlock(x, y, block.GetBlock()); //We're placing this block. Apparently it's the external responsibility to make sure this will work
+        Destroy(block.gameObject); //And now we're done with this GameObject - it's on the grid
+    }
 }
