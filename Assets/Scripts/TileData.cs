734c8852bc2ac329055fc7b77d733756d4051c5e
Maia Doerner <wc7642@gmail.com>
2017-09-20 15:15:49 -0400

    tiledata class

---
+++ Assets/Scripts/TileData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileData : MonoBehaviour {
+
+	public bool isOccupied;
+	public enum TileType
+	{
+		Regular,
+		Vacant,
+		Vestige
+	}
+	public TileType type;
+}
