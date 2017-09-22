1dba952f1039722f1603ce7afe96a97b7fe589a6
Paul Calande <paul.luap09@gmail.com>
2017-09-22 19:42:23 -0400

    created enum for lerp state

--- Assets/Scripts/LerpTo.cs
+++ Assets/Scripts/LerpTo.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 //This class allows us to easily move game objects around
 public class LerpTo : MonoBehaviour
 {
-    //These variables tell us if we're moving...
-    private bool lerpingAtSpeed;
-    private bool lerpingInTime;
+    enum LerpState
+    {
+        None,
+        Speed,
+        Time
+    }
+
+    LerpState state = LerpState.None;
+
     private float movingSpeed;
     private float movingTime;
     private Vector3 movingDestination;
@@ -18,51 +24,54 @@ public class LerpTo : MonoBehaviour
     private float journeyLength;
     private float t; //to keep track of how far we've gone towards our goal if we're timed
 
-    //When this method is called, the GameObject moves towards destination 
-    //at the given speed and will keep moving until it reaches the destination.
-
+    // When this method is called, the GameObject moves towards destination 
+    // at the given speed and will keep moving until it reaches the destination.
     public void LerpToAtSpeed(Vector3 destination, float speed)
     {
-    	MovingSetup();
-	movingDestination = destination;
-        lerpingAtSpeed = true;
+        MovingSetup();
+        movingDestination = destination;
+        state = LerpState.Speed;
         movingSpeed = speed;
     }
 
-	//When this method is called, the GameObject moves towards the destination
-    //and reaches it in the given amount of time (in seconds).
-	public void LerpToInTime(Vector3 destination, float time)
+    // When this method is called, the GameObject moves towards the destination
+    // and reaches it in the given amount of time (in seconds).
+    public void LerpToInTime(Vector3 destination, float time)
     {
         MovingSetup();
-	movingDestination = destination;
-        lerpingInTime = true;
+        movingDestination = destination;
+        state = LerpState.Time;
         movingTime = time;
         t = 0.0f;
     }
 
-    //This method sets all the variables up to start moving
-    private void MovingSetup() 
+    // This method sets all of the variables up to start moving.
+    private void MovingSetup()
     {
-	startTime = Time.time;
-	startPosition = transform.position;
-	journeyLength = Vector3.Distance(gameObject.transform.position, movingDestination);
+        startTime = Time.time;
+        startPosition = transform.position;
+        journeyLength = Vector3.Distance(gameObject.transform.position, movingDestination);
     }
 
     private void Update()
     {
-        //If we've reached our destination, we're done
-        if (movingDestination == transform.position) {
-            lerpingInTime = false;
-            lerpingAtSpeed = false;
+        // If we've reached our destination, we're done.
+        if (movingDestination == transform.position)
+        {
+            state = LerpState.None;
         }
-        if (lerpingAtSpeed == true) {
-            float distCovered = (Time.time - startTime) * movingSpeed;
-            float fracJourney = distCovered / journeyLength;
-            transform.position = Vector3.Lerp(startPosition, movingDestination, fracJourney);
-        }
-        if (lerpingInTime == true) {
-            t += Time.deltaTime / movingTime;
-            transform.position = Vector3.Lerp(startPosition, movingDestination, t);
+        switch (state)
+        {
+            case LerpState.Speed:
+                float distCovered = (Time.time - startTime) * movingSpeed;
+                float fracJourney = distCovered / journeyLength;
+                transform.position = Vector3.Lerp(startPosition, movingDestination, fracJourney);
+                break;
+
+            case LerpState.Time:
+                t += Time.deltaTime / movingTime;
+                transform.position = Vector3.Lerp(startPosition, movingDestination, t);
+                break;
         }
     }
 }
