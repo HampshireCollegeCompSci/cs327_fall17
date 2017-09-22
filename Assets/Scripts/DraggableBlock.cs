cc9dce6da57bd6fcb7b9328dc399d04ac43611cf
Yixiang Xu <yixiangxu@umass.edu>
2017-09-22 14:27:41 -0400

    added function SetBlock  UpdateAvailableSpaces Rotate

--- Assets/Scripts/DraggableBlock.cs
+++ Assets/Scripts/DraggableBlock.cs
@@ -12,9 +12,18 @@ public class DraggableBlock : MonoBehaviour
     Block block;
     bool isDraggable = false;
 
+    Tile[,] tiles;
+    GameObject prefabTile;
+    Grid grid;
+    DraggableObject draggableObject;
+
+    //Copies the data of a Block into this DraggableBlock’s contained Block
     public void SetBlock(Block copiedBlock)
     {
-
+        GameObject tileObject = Instantiate(prefabTile);
+        tiles = TileUtil.CreateTileArray(prefabTile, tileObject.transform.position, copiedBlock.GetWidth(), copiedBlock.GetHeight());
+        block = new Block(copiedBlock.GetWidth(), copiedBlock.GetHeight());
+        UpdateAvailableSpaces();
     }
 
     public Block GetBlock()
@@ -27,13 +36,33 @@ public class DraggableBlock : MonoBehaviour
         isDraggable = true;
     }
 
+    //Retrieve a List of Spaces that the DraggableBlock can fit into and store that List as draggableObject’s 
+    //List of SnapLocations. Use DraggableObject.SetSnapToAreas to pass in the List of SnapLocations.
     void UpdateAvailableSpaces()
     {
+        List<Space> spaces = grid.GetSpacesFree(block.GetWidth(),block.GetHeight(), block);
+        List<SnapLocation> sl = new List<SnapLocation>();
+        for (int i = 0; i < spaces.Count; i++)
+        {
+            sl.Add(spaces[i].GetComponent<SnapLocation>());
+        }
 
+        draggableObject.SetSnapToAreas(sl);
     }
 
+    //Forwards a Rotate call to the underlying Block, then updates the tiles 
+    //array to match the Block’s TileData. Lastly, call UpdateAvailableSpaces.
     public void Rotate(bool clockwise)
     {
-
+        block.Rotate(true);
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            for (int j = 0; j < tiles.GetLength(i); j++)
+            {
+                tiles[i, j].Fill(block.GetTileType(i, j));
+            }
+        }
+       
+        UpdateAvailableSpaces();
     }
 }
