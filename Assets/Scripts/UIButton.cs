0ef075074b60c1db87e61e94ad68554777ca8980
Joel <jjoelesquilin@gmail.com>
2017-09-22 14:12:22 -0400

    UI Buttons. Rotate counterclockwise/clockwise and Reset functions added. Draggableblock and canvas edited in scene.

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIButton : MonoBehaviour {
+
+    public BlockSpawner Spawner;
+	
+    public void RotateClockwise()
+    {
+        Spawner.RotateCurrentBlock(false);  
+    }
+
+    public void RotateCounter()
+    {
+        Spawner.RotateCurrentBlock(true);
+    }
+
+    public void Reset()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
