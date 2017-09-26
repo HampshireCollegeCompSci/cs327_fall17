2a4e5d5e19b67fd02161dcbea284eb906b90d62a
Yixiang Xu <yixiangxu@umass.edu>
2017-09-26 10:34:03 -0400

    implemented function PlacedDraggableBlock, Tile_Changed and EnergyCounter class

--- Assets/Scripts/EnergyCounter.cs
+++ Assets/Scripts/EnergyCounter.cs
@@ -13,19 +13,33 @@ public class EnergyCounter : MonoBehaviour
     [SerializeField]
     [Tooltip("The amount of energy the player currently has.")]
     int energy = 10;
+    [SerializeField]
+    GameFlow gameFlow;
+
+    void Start()
+    {
+        UpdateEnergy();
+    }
 
     public void AddEnergy(int amount)
     {
-
+        energy += amount;
+        UpdateEnergy();
     }
 
     public void RemoveEnergy(int amount)
     {
+        energy -= amount;
+        UpdateEnergy();
 
+        if(energy <= 0)
+        {
+            gameFlow.GameOver(GameFlow.GameOverCause.NoMoreEnergy);
+        }
     }
 
     private void UpdateEnergy()
     {
-
+        textEnergy.text = "Energy: " + energy.ToString();
     }
 }
