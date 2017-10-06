f0312c662e207d9450ffd0c67f43336ceb0d4554
wmjosiah <wjerikson@hampshire.edu>
2017-10-06 14:50:08 -0400

    First basic implementation of analytics

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,38 @@
+﻿//Author: Wm. Josiah Erikson
+//This is the class that will subscribe to the GameLost event and send data up to Unity's servers at that time
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+// Reference the Unity Analytics namespace
+using UnityEngine.Analytics;
+
+public class Analytics : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Reference to the GameFlow instance.")]
+    GameFlow gameFlow;
+    [SerializeField]
+    [Tooltip("Reference to the Score instance.")]
+    ScoreCounter Score;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (gameFlow != null)
+        {
+            gameFlow.GameLost += SendData;
+        }
+
+    }
+
+    void SendData(GameFlow.GameOverCause cause)
+    {
+        int score = Score.GetScore();
+        UnityEngine.Analytics.Analytics.CustomEvent("gameOver", new Dictionary<string, object>
+        {
+            { "score", score },
+    { "gameOverCause", cause }
+  });
+    }
+}
