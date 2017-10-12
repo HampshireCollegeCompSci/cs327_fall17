7072aeaa0983ee9e61fa070ddad6aebac9b9ff9d
Wm. Josiah Erikson <josiah@insanetechnology.com>
2017-10-12 18:07:42 -0400

    Just messing around

---
+++ Assets/Scripts/EnergyMeter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergyMeter : MonoBehaviour {
+    private RectTransform rT; //For storing the rectTransform component
+
+	// Use this for initialization
+	void Start () {
+        rT = GetComponent<RectTransform>(); //This only happens once and prevents things from breaking
+        rT.sizeDelta = new Vector2(rT.sizeDelta.x, rT.sizeDelta.y + 0.5f);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Vector3 ourLocalPosition = transform.localPosition;
+        //rT.sizeDelta = new Vector2(rT.sizeDelta.x, rT.sizeDelta.y + 0.5f);
+        float yPosition = transform.position.y;
+        yPosition += 2.0f;
+        float xPosition = transform.position.x;
+        float zPosition = transform.position.z;
+        //transform.localPosition.Set(yPosition,xPosition,zPosition); //This doesn't work - don't know why. Have to use world space. Seems bad.
+        //        transform.Translate(Vector3.up * 20);
+        Quaternion currentRotation = transform.localRotation;
+        Vector3 newPosition = new Vector3(xPosition, yPosition, zPosition);
+        transform.SetPositionAndRotation(newPosition, currentRotation);
+	}
+}
