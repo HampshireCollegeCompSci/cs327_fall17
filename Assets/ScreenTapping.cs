8ba78f99ea648629a0be9b3f12a67097731d550d
Yixiang Xu <yixiangxu@umass.edu>
2017-10-18 16:33:26 -0400

    added placehold for screen tapping

--- Assets/ScreenTapping.cs
+++ Assets/ScreenTapping.cs
@@ -1,28 +1,27 @@
-﻿using System.Collections;
+﻿// Author(s): Yixiang Xu
+/* This class is working for screen tapping
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using System;
 
-public class ScreenTapping : MonoBehaviour {
+public class ScreenTapping : MonoBehaviour, IPointerDownHandler
+{
     [SerializeField]
     [Tooltip("The prefab to instantiate for ScreenTapping.")]
     GameObject prefabScreenTapping;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        for (int i = 0; i < Input.touchCount; ++i)
-        {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
-            {
-                GameObject TappingAnim = Instantiate(prefabScreenTapping, transform, false);
-                TappingAnim.transform.localPosition = Input.GetTouch(i).position;
-            }
-        }
+    Vector2 localPoint;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPoint);
+        GameObject TappingAnim = Instantiate(prefabScreenTapping, transform, false);
+        TappingAnim.transform.localPosition = localPoint;
+        TappingAnim.GetComponent<Animator>().SetBool("active", true);
     }
 }
