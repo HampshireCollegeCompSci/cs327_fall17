7a92063a1e949cc86a3ce084cf9da65ab1ec4489
Yixiang Xu <yixiangxu@umass.edu>
2017-10-19 14:57:51 -0400

    replaced the dropdown menu with pasue menu

---
+++ Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,28 @@
+﻿// Author(s): Yixiang Xu
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Reference to the pause menu.")]
+    GameObject pauseMenu;
+    [SerializeField]
+    [Tooltip("Reference to the screen tapping controller.")]
+    ScreenTapping screenTapping;
+
+    public void OpenPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        screenTapping.enabled = false;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        screenTapping.enabled = true;
+    }
+}
