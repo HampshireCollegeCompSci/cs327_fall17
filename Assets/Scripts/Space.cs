7a3c5288bce52684b683f174891e0732517f3f10
yifengshi1995 <byoukin@gmail.com>
2017-10-21 01:56:03 -0400

    outlines feature officially implemented. Also added random color for the outlines

--- Assets/Scripts/Space.cs
+++ Assets/Scripts/Space.cs
@@ -68,6 +68,7 @@ public class Space : MonoBehaviour
 	//This method should be called when a DraggableBlock is dragged onto this Space.
 	public void PlaceBlock(DraggableBlock block)
 	{
+        grid.ClearOutline();
         AudioController.Instance.PlaceTile();
         grid.WriteBlock(row, col, block); //We're placing this block. Apparently it's the external responsibility to make sure this will work
         grid.PlacedDraggableBlock(); // Notify the Grid that we just placed a DraggableBlock.
@@ -84,11 +85,11 @@ public class Space : MonoBehaviour
         DraggableBlock draggable = snapper.GetComponent<DraggableBlock>();
         if (grid.SetHighlight(row, col, draggable, on))
         {
-            grid.AnticipatedHighlight(row, col, draggable, true);
+            grid.AnticipatedHighlight(row, col, draggable, true, snapLocation);
         }
         else
         {
-            grid.AnticipatedHighlight(row, col, draggable, false);
+            grid.AnticipatedHighlight(row, col, draggable, false, snapLocation);
         }
     }
 }
