9ebbce3ceb55ac31a34d10f3112d47926ff8d6dc
wmjosiah <wjerikson@hampshire.edu>
2017-10-23 12:29:27 -0400

    Implemented cross-scene place to store things, made it so that Analytics don't send when cheats are enabled, and stopped sending allTheData since it didn't work anyway.

--- Assets/Scripts/RandomSeedModifier.cs
+++ Assets/Scripts/RandomSeedModifier.cs
@@ -1,13 +1,21 @@
-﻿//Sets random seed to 0 for purpose of testing
+﻿//Author(s): Someone, Wm. Josiah Erikson
+//Sets random seed to 0 for purpose of testing
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RandomSeedModifier : MonoBehaviour {
+   
+    Settings settings;
+    private void Awake()
+    {
+        settings = FindObjectOfType<Settings>(); //Can't pass in a reference because it's a singleton
+    }
 
-	public void SetRandomSeedToZero() {
+    public void SetRandomSeedToZero() {
 		Random.InitState (0);
+        settings.setCheatsEnabled(); //Tell the game that cheats have been enabled
 	}
 
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,45 @@
+﻿//Author: Wm. Josiah Erikson
+//This very simple script is for holding persistent cross-scene settings
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour {
+    [SerializeField]
+    [Tooltip("Are cheats enabled? For debug viewing purposes only.")]
+    bool cheatsEnabled; //Are there cheats enabled or not?
+
+    private static Settings instance = null;
+    public static Settings Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+
+    void Awake()
+    {
+        if (instance)
+        {
+            DestroyImmediate(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+    public bool areCheatsEnabled () //Tell us whether cheats are enabled or not
+    {
+        //Debug.Log("Cheats are enabled: " + cheatsEnabled);
+        return cheatsEnabled;
+    }
+
+    public void setCheatsEnabled () //Set cheats enabled
+    {
+        cheatsEnabled = true;
+        //Debug.Log("Cheats are enabled: " + cheatsEnabled);
+    }
+}
