f58c80f792f1f5eff567b0e1f3b41452a4051e34
Yixiang Xu <yixiangxu@umass.edu>
2017-10-24 19:25:52 -0400

    added trail effect to screentapping

--- Assets/Scripts/ScreenTapping.cs
+++ Assets/Scripts/ScreenTapping.cs
@@ -9,13 +9,34 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System;
 
-public class ScreenTapping : MonoBehaviour, IPointerDownHandler
+public class ScreenTapping : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     [SerializeField]
     [Tooltip("The prefab to instantiate for ScreenTapping.")]
     GameObject prefabScreenTapping;
 
     Vector2 localPoint;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPoint);
+        GameObject tappingAnim = Instantiate(prefabScreenTapping, transform, false);
+        tappingAnim.transform.localPosition = localPoint;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPoint);
+        GameObject tappingAnim = Instantiate(prefabScreenTapping, transform, false);
+        tappingAnim.transform.localPosition = localPoint;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPoint);
+        GameObject tappingAnim = Instantiate(prefabScreenTapping, transform, false);
+        tappingAnim.transform.localPosition = localPoint;
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
