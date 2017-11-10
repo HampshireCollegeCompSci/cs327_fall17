ee469bc8a5000087e141650ccec28f227b0d7b41
Maia Doerner <wc7642@gmail.com>
2017-11-10 15:42:56 -0500

    fading screen tap

--- Assets/Scripts/Animations/ScreenTappingBehavior.cs
+++ Assets/Scripts/Animations/ScreenTappingBehavior.cs
@@ -18,7 +18,7 @@ public class ScreenTappingBehavior : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-        Destroy(animator.gameObject);
+        //Destroy(animator.gameObject);
 	}
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
