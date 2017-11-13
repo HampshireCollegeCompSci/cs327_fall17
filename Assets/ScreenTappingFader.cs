ede35951a6acf7dc0c7aa57684e39c5dcea628e3
Paul Calande <paul.luap09@gmail.com>
2017-11-12 19:09:17 -0500

    some small tweaks

--- Assets/ScreenTappingFader.cs
+++ Assets/ScreenTappingFader.cs
@@ -1,55 +1,54 @@
-﻿using System.Collections;
+﻿// Author(s): Maia Doerner, Paul Calande
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ScreenTappingFader : MonoBehaviour {
+public class ScreenTappingFader : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("How many seconds it takes for the particle to vanish.")]
+    float fadeTime = 0.8f;
+    [SerializeField]
+    [Tooltip("How quickly the particle moves.")]
+    float movementSpeed = 3.0f;
+    [SerializeField]
+    [Tooltip("How many degrees the particle rotates per second.")]
+    float rotationSpeed = 180.0f;
+    [SerializeField]
+    [Tooltip("Reference to the Image component.")]
+    Image image;
 
-	float fadeTime = .8f;
-	Image image;
-	Color imageColor;
-	float randomRotate;
-	float randomPosition;
-	float speed;
-	Vector3 direction;
+    Color imageColor;
+    float randomRotate;
+    float randomPosition;
+    Vector3 direction;
+    float elapsedTime = 0.0f;
 
-	void Awake () 
-	{
-		//fadetTime = 1f;
-		image = GetComponent<Image>();
-		imageColor = image.color;
-		StartCoroutine (FadeOut (image));
-		randomRotate = Random.Range(0, 5);
-		randomPosition = Random.Range(0, 50);
-		float x = Random.Range(-1f, 1f);
-		float y = Random.Range(-1f, 1f);
-		direction = new Vector3(x, y, 0f);
-		speed = 5f;
-	}
+    void Awake()
+    {
+        imageColor = image.color;
+        randomRotate = Random.Range(0.0f, 360.0f);
+        transform.Rotate(0.0f, 0.0f, randomRotate);
+        float x = Random.Range(-1.0f, 1.0f);
+        float y = Random.Range(-1.0f, 1.0f);
+        direction = new Vector3(x, y, 0.0f);
+    }
 
-	void Update () 
-	{
-		
-	}
-
-	private YieldInstruction fadeInstruction = new YieldInstruction();
-	IEnumerator FadeOut(Image image)
-	{
-		float elapsedTime = 0.0f;
-		while (elapsedTime < fadeTime)
-		{
-			yield return fadeInstruction;
-			elapsedTime += Time.deltaTime ;
-			imageColor.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
-			image.color = imageColor;
-			Debug.Log (imageColor);
-			if (imageColor.a == 0) {
-				Destroy (gameObject);
-			}
-			transform.Rotate(Time.deltaTime * 5, Time.deltaTime * 5, 0);
-			transform.position += direction * speed;
-
-		}
-	} 
-
+    void Update()
+    {
+        if (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            imageColor.a = 1.0f - Mathf.Clamp01(elapsedTime / fadeTime);
+            image.color = imageColor;
+            transform.Rotate(0, 0, Time.deltaTime * rotationSpeed);
+            transform.position += direction * movementSpeed;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 }
