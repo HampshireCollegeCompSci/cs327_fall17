9ea29c54bd61eac695b9666af93ecd2225af0316
yifengshi1995 <byoukin@gmail.com>
2017-11-14 03:23:50 -0500

    explosion effect implemented; lightning effect changed

---
+++ Assets/Scripts/Animations/ClearExplosionBehavior.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearExplosionBehavior : MonoBehaviour {
+
+	public void DestroyMe()
+    {
+        Destroy(gameObject);
+    }
+}
