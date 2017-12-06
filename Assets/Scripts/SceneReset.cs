7ef3437bad86a2b5d8ccb7bc6a2880f6a4d76150
Joel <jjoelesquilin@gmail.com>
2017-12-06 11:48:02 -0500

    Reset window confirmation

--- Assets/Scripts/SceneReset.cs
+++ Assets/Scripts/SceneReset.cs
@@ -8,11 +8,27 @@ using UnityEngine.SceneManagement;
 
 public class SceneReset : MonoBehaviour
 {
+
+    public GameObject confirmPanel;
+
     public void ResetScene()
     {
-        AudioController.Instance.MenuClick();
 
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.name);
+    }
+
+    public void OpenMenu()
+    {
+        AudioController.Instance.MenuClick();
+
+        confirmPanel.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        AudioController.Instance.MenuClick();
+
+        confirmPanel.SetActive(false);
     }
 }
