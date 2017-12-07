6735d3c33c02821246a451f7c8ec7af103224b2d
Paul Calande <paul.luap09@gmail.com>
2017-12-07 18:34:07 -0500

    The score block UI is no longer present in tutorial mode.

---
+++ Assets/Scripts/UI/DisableInModes.cs
@@ -0,0 +1,21 @@
+﻿// Author(s): Paul Calande
+// Disables gameObject in certain game modes.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableInModes : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Whether this object is enabled in the tutorial.")]
+    bool enabledInTutorial;
+
+    private void Awake()
+    {
+        if (!enabledInTutorial && Settings.Instance.IsTutorialModeEnabled())
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
