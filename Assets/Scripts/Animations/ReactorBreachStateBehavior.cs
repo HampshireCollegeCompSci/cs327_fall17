359ec21a338dde0a550b317ceb4a391106b4a8be
史一锋 <byoukin@gmail.com>
2017-12-08 14:51:17 -0500

    reactor breach animation loop only once

---
+++ Assets/Scripts/Animations/ReactorBreachStateBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReactorBreachStateBehavior : StateMachineBehaviour {
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animator.SetBool("Finished", true);
+        animator.enabled = false;
+    }
+}
