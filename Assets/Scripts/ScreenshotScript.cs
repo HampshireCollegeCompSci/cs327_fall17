39600f4c6da9fd1c4e7f380bfe44a3ee74bf21b1
wmjosiah <wjerikson@hampshire.edu>
2017-12-13 12:27:49 -0500

    Added some

---
+++ Assets/Scripts/ScreenshotScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotScript: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //if (Input.GetMouseButton(0)) {
+        //    ScreenCapture.CaptureScreenshot("Screenshot-" + Time.time + ".png");
+        //    Debug.Log("Captured Screenshot");
+        //}
+		
+	}
+    public void TakeScreenshot(){
+        ScreenCapture.CaptureScreenshot("Screenshot-" + Time.time + ".png");
+        Debug.Log("Captured Screenshot");
+    }
+    //void OnMouseDown()
+    //{
+    //    ScreenCapture.CaptureScreenshot("Screenshot.png");
+    //    Debug.Log("Captured Screenshot");
+    //}
+}
